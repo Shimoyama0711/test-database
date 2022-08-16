d95c6d1de6213ecda5e81b25f44415032939b7cc
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2022-08-16 11:48:16 +0900

    test supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,5 @@
-import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-serve((req: Request) => new Response("Hello World"));
+serve(async (req) => {
+    return new Response("Not Found", { status: 404 });
+});
